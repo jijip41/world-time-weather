b06d66376d7b075d35eac1b0f1456efa7c160f94
Jihye Pak <jiji.pak41@gmail.com>
2021-08-13 02:15:34 +0100

    js slice data to present by unit

--- script.js
+++ script.js
@@ -23,5 +23,35 @@ function createContent(e) {
       const index = data.indexOf('T');
       const date_now = data.slice(0, index);
       const time_now = data.slice(index + 1, 19);
+
+      // let hour = realTime()
+      // let min = realTime()
+      // let sec = realTime(Number(time_now.slice(6)));
+
+      dom_frag.querySelector('.date__current').textContent = date_now;
+      dom_frag.querySelector('.hour__num').textContent = time_now.slice(0, 2);
+      dom_frag.querySelector('.min__num').textContent = time_now.slice(3, 5);
+      dom_frag.querySelector('.sec__num').textContent = time_now.slice(6, 8);
+      console.log(date_now);
+      console.log(time_now);
+
+      document.querySelector('#template__container').append(dom_frag);
+
+      // time passed
+
+      let count_time = 0;
+      setInterval(() => {}, 1000);
+
+      form.addEventListener('submit', createContent);
+
+      // function for real time
+
+      // function countSec(num){
+      //   setInterval((num) => {
+      //     num++
+      //     return num;
+      //   }, 1000);
+
+      // }
     });
 }
