720cf9479617432fe0231cbd95cc54dc78743189
jihye <jiji.pak41@gmail.com>
2021-08-14 23:58:34 +0100

    test

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 'use strict';
 
+// Get time data
 const form = document.querySelector('form');
 const container = document.querySelector('#template__container');
 
-function createContent(e) {
-  e.preventDefault();
-
-  const form_data = new FormData(form);
-  const area = form_data.get('area');
-  const city = form_data.get('city').replace(' ', '_');
-
+function createTime(e) {
   const template = document.querySelector('#template');
   const dom_frag = template.content.cloneNode(true);
+  const form_time_data = new FormData(form);
+  const area = form_time_data.get('area');
+  const city = form_time_data.get('city').replace(' ', '_');
+  if (area && city) {
+    fetch(`http://worldtimeapi.org/api/timezone/${area}/${city}`)
+      .then((response) => response.json())
+      .then((json) => {
+        return json.datetime;
+      })
+      .then((data) => {
+        const index = data.indexOf('T');
+        const date_now = data.slice(0, index);
+        const time_now = data.slice(index + 1, 19);
 
-  fetch(`http://worldtimeapi.org/api/timezone/${area}/${city}`)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json.datetime);
-      return json.datetime;
-    })
-    .then((data) => {
-      const index = data.indexOf('T');
-      const date_now = data.slice(0, index);
-      const time_now = data.slice(index + 1, 19);
-
-      // let hour = realTime()
-      // let min = realTime()
-      // let sec = realTime(Number(time_now.slice(6)));
-
-      dom_frag.querySelector('.date__current').textContent = date_now;
-      dom_frag.querySelector('.hour__num').textContent = time_now.slice(0, 2);
-      dom_frag.querySelector('.min__num').textContent = time_now.slice(3, 5);
-      dom_frag.querySelector('.sec__num').textContent = time_now.slice(6, 8);
-      console.log(date_now);
-      console.log(time_now);
-
-      document.querySelector('#template__container').append(dom_frag);
-
-      // time passed
-
-      let count_time = 0;
-      setInterval(() => {}, 1000);
-
-      form.addEventListener('submit', createContent);
-
-      // function for real time
-
-      // function countSec(num){
-      //   setInterval((num) => {
-      //     num++
-      //     return num;
-      //   }, 1000);
-
-      // }
-    });
+        dom_frag.querySelector('.date__current').textContent = date_now;
+        dom_frag.querySelector('.hour__num').textContent = time_now.slice(0, 2);
+        dom_frag.querySelector('.min__num').textContent = time_now.slice(3, 5);
+        dom_frag.querySelector('.sec__num').textContent = time_now.slice(6, 8);
+        document.querySelector('#template__container').append(dom_frag);
+      });
+  } else {
+    alert('input!');
+  }
 }
 
-form.addEventListener('submit', createContent);
+// get weather data
+function createWeather() {
+  const form_weather_data = new FormData(form);
+  const weather_city = capitalise(form_weather_data.get('city'));
+
+  console.log(weather_city);
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${weather_city}&appid=45721c1bf9a8f0380c854c9ef9cfd90a`
+  ).then((json) => {
+    console.log(json);
+    return json;
+  });
+
+  // document.querySelector('#template__container').append(dom_frg);
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  createTime();
+  createWeather();
+});
+
+function capitalise(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
