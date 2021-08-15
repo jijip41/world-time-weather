40cc8b1893aeab193fc0f7d9040ac49fb19a0654
jihye <jiji.pak41@gmail.com>
2021-08-15 23:20:16 +0100

    update weather api

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 'use strict';
 
 // Get time data
-const form = document.querySelector('form');
-const container = document.querySelector('#template__container');
 
-function createTime(e) {
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const container = document.querySelector('#template__container');
   const template = document.querySelector('#template');
   const dom_frag = template.content.cloneNode(true);
+
   const form_time_data = new FormData(form);
   const area = form_time_data.get('area');
   const city = form_time_data.get('city').replace(' ', '_');
+
   if (area && city) {
     fetch(`https://worldtimeapi.org/api/timezone/${area}/${city}`)
       .then((response) => response.json())
@@ -25,35 +30,33 @@ function createTime(e) {
         dom_frag.querySelector('.hour__num').textContent = time_now.slice(0, 2);
         dom_frag.querySelector('.min__num').textContent = time_now.slice(3, 5);
         dom_frag.querySelector('.sec__num').textContent = time_now.slice(6, 8);
-        document.querySelector('#template__container').append(dom_frag);
       });
   } else {
     alert('input!');
   }
-}
 
-// get weather data
-function createWeather() {
+  // get weather data
+
   const form_weather_data = new FormData(form);
   const weather_city = capitalise(form_weather_data.get('city'));
 
-  console.log(weather_city);
-
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${weather_city}&appid=45721c1bf9a8f0380c854c9ef9cfd90a`
-  ).then((json) => {
-    console.log(json);
-    return json;
-  });
+    `https://api.weatherbit.io/v2.0/current?city=${weather_city}&key=ef858dae1f9649658ab6ae42c345aa36&include=minutely`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      return json.data[0].weather.icon;
+    })
+    .then((data) => {
+      let weather_icon = data;
+      dom_frag.querySelector(
+        '.weather__img'
+      ).src = `https://www.weatherbit.io/static/img/icons/${weather_icon}.png`;
 
-  // document.querySelector('#template__container').append(dom_frg);
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  createTime();
-  createWeather();
+      container.append(dom_frag);
+    });
 });
 
 function capitalise(string) {
